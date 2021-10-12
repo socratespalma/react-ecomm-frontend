6790968fc2fb21ff0194dc4c349fe60cbaf9d9bb
Socrates <dubstepero10@gmail.com>
2021-10-12 16:05:21 -0600

    agregando cambios de signup y sidepanel

--- src/views/auth/Signup.js
+++ src/views/auth/Signup.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import SidePanel from "../../components/SidePanel";
 
 const Signup = () => {
   return (
     <main className="trick">
       <Header />
-      <h1>This is signup page</h1>
+      <section className="main-signup-section">
+        <SidePanel />
+        <section className="signup-section">
+          <h1>Registrarse</h1>
+          <h2>¿Ya tienes una cuenta? Inicia Sesión</h2>
+          <div className="signup-section__form">
+            <input type="text" placeholder="Usuario" />
+            <input type="email" placeholder="Correo" />
+            <input type="password" placeholder="Contraseña" />
+            <button>Registrarse</button>
+          </div>
+          <p>
+            Al dar click en "Registrarse" estas de acuerdo con los términos y
+            condiciones de uso
+          </p>
+        </section>
+      </section>
       <Footer />
     </main>
   );
