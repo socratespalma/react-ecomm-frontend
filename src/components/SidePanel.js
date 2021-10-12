0eabe4311f08080405d855d7a22f4607e9ecb4af
Socrates <dubstepero10@gmail.com>
2021-10-11 22:13:04 -0600

    adding sidepanel to login page

---
+++ src/components/SidePanel.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SidePanel() {
+  return (
+    <div className="main-sidepanel">
+      <figure>
+        <img src={require("../assets/images/Logo.png").default} alt="logo" />
+      </figure>
+    </div>
+  );
+}
--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import SidePanel from "../../components/SidePanel";
 
 const Login = () => {
   return (
     <main className="trick">
       <Header />
-      <h1>This is login page</h1>
+      <section className="main-loginsection">
+        <SidePanel />
+        <div>
+          <h1>This is login page</h1>
+        </div>
+      </section>
+
       <Footer />
     </main>
   );
