937f197a6e3c9824cbd759d02c3cce3fcaa56e9b
luis-vivas <luisvivas10@gmail.com>
2021-10-12 18:33:15 -0600

    🚀 deploy stuff

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -3,8 +3,12 @@ import React from "react";
 export default function SidePanel() {
   return (
     <div className="main-sidepanel">
-      <figure>
-        <img src={require("../assets/images/Logo.png").default} alt="logo" />
+      <figure className="main-sidepanel__imgcontainer">
+        <img
+          src={require("../assets/logos/Brand_logo.png").default}
+          className="main-sidepanel__img"
+          alt="logo"
+        />
       </figure>
     </div>
   );
