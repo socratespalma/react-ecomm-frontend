2d1209ec2d2a0107fc082afb372cc36d74961f90
Socrates <dubstepero10@gmail.com>
2021-10-14 13:59:16 -0600

    adding signup logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./styles/main.scss";
 import AppRouter from "./navigation/AppRouter";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.post["Accept"] = "application/json";
+
+// To generate csrf token
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
--- src/views/auth/Signup.js
+++ src/views/auth/Signup.js
@@ -1,30 +1,72 @@
+import axios from "axios";
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import { Link, useHistory } from "react-router-dom";
+// import PropTypes from "prop-types";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import SidePanel from "../../components/SidePanel";
 
-const Signup = ({ onSignup }) => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [passwd, setPasswd] = useState("");
+// const Signup = ({ onSignup }) => {
+//   const [username, setUsername] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [passwd, setPasswd] = useState("");
 
-  const onSubmit = (e) => {
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (!email || !passwd) {
+//       alert("Please add a valid user");
+//       return;
+//     }
+
+//     onSignup({ username, email, passwd });
+
+//     setUsername("");
+//     setEmail("");
+//     setPasswd("");
+//   };
+
+function Signup() {
+  const history = useHistory();
+  const [registerInput, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error_list: [],
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setRegister({ ...registerInput, [e.target.name]: e.target.value });
+  };
+
+  const registerSubmit = (e) => {
     e.preventDefault();
 
-    if (!email || !passwd) {
-      alert("Please add a valid user");
-      return;
-    }
+    const data = {
+      name: registerInput.name,
+      email: registerInput.email,
+      password: registerInput.password,
+    };
 
-    onSignup({ username, email, passwd });
-
-    setUsername("");
-    setEmail("");
-    setPasswd("");
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      axios.post(`/api/register`, data).then((res) => {
+        if (res.data.status === 200) {
+          localStorage.setItem("auth_token", res.data.token);
+          localStorage.setItem("auth_name", res.data.username);
+          alert("Success");
+          history.push("/");
+        } else {
+          setRegister({
+            ...registerInput,
+            error_list: res.data.validation_errors,
+          });
+        }
+      });
+    });
   };
+
   return (
     <main className="trick">
       <Header />
@@ -33,14 +75,16 @@ const Signup = ({ onSignup }) => {
         <section className="signup-section">
           <div className="signup form-container">
             <h1 className="signup__title"> Registrarse </h1>
-            <form className="login__form" onSubmit={onSubmit}>
+            <form className="login__form" onSubmit={registerSubmit}>
               <div className="form-group">
                 <input
                   placeholder="Ingrese Usuario"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
+                  name="name"
+                  onChange={handleInput}
+                  value={registerInput.name}
                   required
                 />
+                <span>{registerInput.error_list.name}</span>
                 <label class="control-label" for="input">
                   Usuario
                 </label>
@@ -51,10 +95,12 @@ const Signup = ({ onSignup }) => {
                 <input
                   type="email"
                   placeholder="user@ecomm.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  name="email"
+                  onChange={handleInput}
+                  value={registerInput.email}
                   required
                 />
+                <span>{registerInput.error_list.email}</span>
                 <label class="control-label" for="input">
                   Correo
                 </label>
@@ -65,10 +111,12 @@ const Signup = ({ onSignup }) => {
                 <input
                   type="password"
                   placeholder="Ingrese la contraseña"
-                  value={passwd}
-                  onChange={(e) => setPasswd(e.target.value)}
+                  name="password"
+                  onChange={handleInput}
+                  value={registerInput.password}
                   required
                 />
+                <span>{registerInput.error_list.password}</span>
                 <label class="control-label" for="input">
                   Contraseña
                 </label>
@@ -94,10 +142,12 @@ const Signup = ({ onSignup }) => {
       <Footer />
     </main>
   );
-};
+}
 
-Signup.propTypes = {
-  onLogin: PropTypes.func,
-};
+// };
+
+// Signup.propTypes = {
+//   onLogin: PropTypes.func,
+// };
 
 export default Signup;
