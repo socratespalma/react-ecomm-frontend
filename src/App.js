4d9a54f7116d4f2f8a2e2858339bef07e582ae6f
luis-vivas <luisvivas10@gmail.com>
2021-10-14 14:57:25 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ axios.defaults.baseURL = "http://127.0.0.1:8000";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 
-// To generate csrf token
+// To generate csrf token adsfdsgafg
 axios.defaults.withCredentials = true;
 
 function App() {
