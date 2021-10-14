e41312ba78146e0eeb0523a05be81bfc2899b872
luis-vivas <luisvivas10@gmail.com>
2021-10-14 14:49:46 -0600

    🐛 base URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./styles/main.scss";
 import AppRouter from "./navigation/AppRouter";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "http://127.0.0.1:8000";
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 
