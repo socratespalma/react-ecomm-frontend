7eebdc660c62f5fbd3c69d7bb977c71473f60e78
Socrates <dubstepero10@gmail.com>
2021-10-14 18:01:26 -0600

    adding login and signup logic

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,30 +1,70 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+import { Link, useHistory } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import SidePanel from "../../components/SidePanel";
+import axios from "axios";
 
-const Login = ({ onLogin }) => {
-  const [email, setEmail] = useState("");
-  const [passwd, setPasswd] = useState("");
-  const [reminder, setReminder] = useState("");
+// const Login = ({ onLogin }) => {
+//   const [email, setEmail] = useState("");
+//   const [passwd, setPasswd] = useState("");
+//   const [reminder, setReminder] = useState("");
 
-  const onSubmit = (e) => {
+//   const onSubmit = (e) => {
+//     e.preventDefault();
+
+//     if (!email || !passwd) {
+//       alert("Please add a valid user");
+//       return;
+//     }
+
+//     onLogin({ email, passwd, reminder });
+
+//     setEmail("");
+//     setPasswd("");
+//     setReminder(false);
+//   };
+function Login() {
+  const history = useHistory();
+  const [loginInput, setLogin] = useState({
+    email: "",
+    password: "",
+    error_list: [],
+  });
+
+  const handleInput = (e) => {
+    e.persist();
+    setLogin({ ...loginInput, [e.target.name]: e.target.value });
+  };
+
+  const loginSubmit = (e) => {
     e.preventDefault();
 
-    if (!email || !passwd) {
-      alert("Please add a valid user");
-      return;
-    }
+    const data = {
+      email: loginInput.email,
+      password: loginInput.password,
+    };
 
-    onLogin({ email, passwd, reminder });
-
-    setEmail("");
-    setPasswd("");
-    setReminder(false);
+    axios.get("/sanctum/csrf-cookie").then((response) => {
+      axios.post(`/api/login`, data).then((res) => {
+        if (res.data.status === 200) {
+          localStorage.setItem("auth_token", res.data.token);
+          localStorage.setItem("auth_name", res.data.username);
+          alert("Success");
+          history.push("/");
+        } else if (res.data.status === 401) {
+          alert("Error");
+        } else {
+          setLogin({
+            ...loginInput,
+            error_list: res.data.validation_errors,
+          });
+        }
+      });
+    });
   };
+
   return (
     <main className="trick">
       <Header />
@@ -33,15 +73,17 @@ const Login = ({ onLogin }) => {
         <div className="login-section">
           <div className="login form-container">
             <h1 className="login__title"> Iniciar Sesión </h1>
-            <form className="login__form" onSubmit={onSubmit}>
+            <form className="login__form" onSubmit={loginSubmit}>
               <div className="form-group">
                 <input
                   type="email"
                   placeholder="user@ecomm.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
+                  name="email"
+                  onChange={handleInput}
+                  value={loginInput.email}
                   required
                 />
+                <span>{loginInput.error_list.email}</span>
                 <label class="control-label" for="input">
                   Correo
                 </label>
@@ -52,10 +94,12 @@ const Login = ({ onLogin }) => {
                 <input
                   type="password"
                   placeholder="Ingrese la contraseña"
-                  value={passwd}
-                  onChange={(e) => setPasswd(e.target.value)}
+                  name="password"
+                  onChange={handleInput}
+                  value={loginInput.password}
                   required
                 />
+                <span>{loginInput.error_list.password}</span>
                 <label class="control-label" for="input">
                   Contraseña
                 </label>
@@ -66,9 +110,7 @@ const Login = ({ onLogin }) => {
                 <label>
                   <input
                     type="checkbox"
-                    checked={reminder}
-                    value={reminder}
-                    onChange={(e) => setReminder(e.currentTarget.checked)}
+                    // onChange={(e) => setReminder(e.currentTarget.checked)}
                   />
                   <i class="helper"></i>
                   Recordar usuario
@@ -91,10 +133,12 @@ const Login = ({ onLogin }) => {
       <Footer />
     </main>
   );
-};
+}
 
-Login.propTypes = {
-  onLogin: PropTypes.func,
-};
+// };
+
+// Login.propTypes = {
+//   onLogin: PropTypes.func,
+// };
 
 export default Login;
