741085b89bcafc2f561daeb592dd26b09c22e4b3
luis-vivas <luisvivas10@gmail.com>
2021-12-01 17:28:57 -0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import "./styles/main.scss";
-import AppRouter from "./navigation/AppRouter";
-import axios from "axios";
+import './styles/main.scss'
+import AppRouter from './navigation/AppRouter'
+import axios from 'axios'
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.headers.post["Accept"] = "application/json";
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.headers.post['Content-Type'] = 'application/json'
+axios.defaults.headers.post['Accept'] = 'application/json'
 
 // To generate csrf token adsfdsgafg
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = true
 axios.interceptors.request.use(function (config) {
-  const token = localStorage.getItem("auth_token");
-  config.headers.Authorization = token ? `Bearer ${token}` : "";
-  return config;
-});
+	const token = localStorage.getItem('auth_token')
+	config.headers.Authorization = token ? `Bearer ${token}` : ''
+	return config
+})
 
 function App() {
-  return (
-    <div className="App">
-      <AppRouter />
-    </div>
-  );
+	return (
+		<>
+			<AppRouter />
+		</>
+	)
 }
 
-export default App;
+export default App
--- src/views/common/Products.js
+++ src/views/common/Products.js
@@ -1,73 +1,71 @@
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import SidePanel from "../../components/SidePanel";
-import ProductsList from "../../components/products/ProductsList";
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+import SidePanel from '../../components/SidePanel'
+import ProductsList from '../../components/products/ProductsList'
 
 const Products = () => {
-  const products = [
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-    {
-      img: "../../assets/images/Calcetas.jpg",
-      name: "Calcetas Arcoiris",
-      price: "$12.00",
-    },
-  ];
-  return (
-    <div className="trick">
-      <Header />
-      <main className="main">
-        <SidePanel />
-        <section className="product-section">
-          <ProductsList products={products} />
-        </section>
-      </main>
-      <Footer />
-    </div>
-  );
-};
+	const products = [
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+		{
+			img: './images/Mochila.jpg',
+			name: 'Calcetas Arcoiris',
+			price: '$12.00',
+		},
+	]
+	return (
+		<>
+			<Header />
+			<main className='main'>
+				<SidePanel />
+				<ProductsList products={products} />
+			</main>
+			<Footer />
+		</>
+	)
+}
 
-export default Products;
+export default Products
